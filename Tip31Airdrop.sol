ae20f5f64282fa52c25e16ce66e79e9daddeb91f
Sara <spajcin8@gmail.com>
2022-07-11 13:43:23 +0200

    add Tip31 MultiTransfer method

--- Tip31Airdrop.sol
+++ Tip31Airdrop.sol
@@ -6,14 +6,21 @@ contract Tip31Airdrop {
 
     address _senderAddr;
     address _tokenRootAddr;
+    address[] _recipients;
+    uint128[] _amounts;
+
     uint128 constant private MAX_TRANSFER_NUMBER = 100000;
     uint128 private transferNumber = 255;
+
     address private walletAddress;
+    uint128 private deposit = 0;
+
     uint128 private transferGas = 0.8 ever;
     uint128 private transactionFee = 0.015 ever;
     uint total_amount = 0;
 
-    constructor(address senderAddr, address tokenRootAddr) public 
+    constructor(address senderAddr, address tokenRootAddr,
+    address[] recipients, uint128[] amounts) public 
     {
         require(senderAddr.value != 0, 1001);
         require(tokenRootAddr.value != 0, 1001);
@@ -21,50 +28,48 @@ contract Tip31Airdrop {
 
         _senderAddr = senderAddr;
         _tokenRootAddr = tokenRootAddr;
+        _recipients = recipients;
+        _amounts = amounts;
+
+        walletAddress = senderAddr;
+        uint128 amount = expectTotalAmount(amounts);
+        deposit = deposit + amount;
     }
 
-    // function multiTransfer(address[] recipients, uint128[] amounts) balanceSufficient external {
-    //     require(recipients.length > 0 && recipients.length <= transferNumber, 1002, "The number of recipients error!");
-    //     require(recipients.length == amounts.length, 1003, "The number of recipients must equal to the number of amounts!");
-    //     tvm.accept();
+    function multiTransfer() external {
+        require(walletAddress.value != 0, 1001, "Wallet address error!");
+        require(_recipients.length > 0 && _recipients.length <= transferNumber, 1002, "The number of recipients error!");
+        require(_recipients.length == _amounts.length, 1003, "The number of recipients must equal to the number of amounts!");
+        tvm.accept();
 
-    //     for (uint i=0; i < amounts.length; i++) {
-    //         total_amount += amounts[i];
-    //     }
+        uint128 msgValue = uint128(msg.value);
+        require(msgValue >= _amounts.length * (transferGas + transactionFee), 1008, "not sufficient gas!");
 
-    //     walletAddress = msg.sender;
+        uint128 totalAmount = expectTotalAmount(_amounts);
+        require(deposit >= totalAmount, 1007, "not sufficient funds!");
 
-    //     startTransfer(recipients, amounts, _senderAddr);
-    // }
+        startTransfer(_recipients, _amounts, _senderAddr);
+    }
 
-    // function startTransfer(address[] recipients, uint128[] amounts, address remainingGasTo) private view {  
-    //     require(walletAddress.value != 0, 1001, "Wallet address error!");
+    function startTransfer(address[] recipients, uint128[] amounts, address remainingGasTo) private {
+        TIP31TokenWallet bridgeWallet = TIP31TokenWallet(walletAddress);
+        TvmCell _empty;
+        for (uint128 i = 0; i < recipients.length; i++) {
+            address recipient = recipients[i];
+            uint128 amount = amounts[i];
+            require(deposit >= amount, 1007, "not sufficient funds!");
+            bridgeWallet.transfer{value: transferGas, flag: 0}(amount, recipient, 0.5 ever, remainingGasTo, false, _empty);
+            deposit = deposit - amount;
+        }
+    }
 
-    //     TIP31TokenWallet bridgeWallet = TIP31TokenWallet(walletAddress);
-    //     TvmCell _empty;
-        
-    //     for (uint128 i = 0; i < recipients.length; i++) {
-    //         address recipient = recipients[i];
-    //         uint128 amount = amounts[i];
-    //         bridgeWallet.transfer{value: transferGas, flag: 0}(amount, recipient, 0.5 ever, remainingGasTo, false, _empty);
-    //     }
-    // }
+    function expectTotalAmount(uint128[] amounts) public returns (uint128) {
+        require(amounts.length > 0 && amounts.length <= transferNumber, 1002, "The number of amounts error!");
+        uint128 totalAmount;
+        for (uint128 i = 0; i < amounts.length; i ++) {
+            totalAmount += amounts[i];
+        }
+        return totalAmount;
+    }
 
-    // function expectTotalAmount(uint128[] amounts) public view returns (uint128) {
-    //     require(amounts.length > 0 && amounts.length <= transferNumber, 1002, "The number of amounts error!");
-    //     uint128 totalAmount;
-    //     for (uint128 i = 0; i < amounts.length; i ++) {
-    //         totalAmount += amounts[i];
-    //     }
-    //     return totalAmount;
-    // }
-   
-    // 1 TON should be enough for any possible fees
-    // The rest of the balance can always be refunded
-    // modifier balanceSufficient {
-    //     require(address(this).balance > total_amount + transactionFee, 105);
-    //     tvm.accept();
-
-    //     _;
-    // }
  }
