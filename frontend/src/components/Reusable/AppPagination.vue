a0e61c756377729f9fd9caef3bbdd32a6879043b
milos-markovic <milosgra@gmail.com>
2022-10-28 13:26:37 +0200

    css fixes (#4)

--- frontend/src/components/Reusable/AppPagination.vue
+++ frontend/src/components/Reusable/AppPagination.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="w-full font-pt_root text-[14px]">
     <div class="flex items-center justify-end space-x-[20px]">
-      <div class="hidden md:flex items-center space-x-[6px]">
-        <form>
-          <select v-model="itemsPerPage" class="pagination-dropdown">
-            <option v-for="n in 50" :key="n" :value="n">{{ n }}</option>
+      <div class="hidden md:flex items-center space-x-[6px] ">
+        <form class="inline-block relative space-x-3 w-[62px]">
+          <select v-model="itemsPerPage" class="pagination-dropdown block appearance-none">
+            <option v-for="n in 50" :key="n" :value="n">{{ n }}</option>   
           </select>
+          <div class="pointer-events-none absolute top-1/2 right-4 -translate-y-1/2">
+            <svg  width="10" height="5" viewBox="0 0 10 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5 5L0 0H10L5 5Z" fill="#B7BAC2"/>
+            </svg>         
+          </div>
         </form>
         <span> items on page </span>
         <!--        <span> items on page of ... </span>-->
@@ -27,7 +32,7 @@
 
       <div class="paginationBtns">
         <button :disabled="walletStore.currentPage === 1" @click="onPrevPage">
-          <div class="leftBtn w-[36px] h-[36px] flex items-center justify-center bg-[#ECF1FE]">
+          <div class="leftBtn w-[36px] h-[36px] flex items-center justify-center bg-[#ECF1FE] hover:bg-blue-100">
             <LeftArrowIcon />
           </div>
         </button>
@@ -36,7 +41,7 @@
           :disabled="airdropStore.airdrops ? airdropStore.airdrops.continuation === undefined : false"
           @click="onNextPage"
         >
-          <div class="rightBtn w-[36px] h-[36px] flex items-center justify-center bg-[#ECF1FE]">
+          <div class="rightBtn w-[36px] h-[36px] flex items-center justify-center bg-[#ECF1FE] hover:bg-blue-100">
             <RightArrowIcon />
           </div>
         </button>
